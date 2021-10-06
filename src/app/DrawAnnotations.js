8b6841c4fb9dacdb293a468df72f965689e62251
Priyanshu Kumar <nix0145@gmail.com>
2021-10-06 08:43:03 +0530

    fc2

--- src/app/DrawAnnotations.js
+++ src/app/DrawAnnotations.js
@@ -3,45 +3,50 @@ import {Arrow, Group, Layer, Rect, Stage, Text} from "react-konva";
 
 const DrawAnnotations = () => {
         const [annotations, setAnnotations] = useState([]);
-        const [newAnnotation, setNewAnnotation] = useState([]);
-        const [arrows, setArrows] = useState([]);
+        const [newRect, setNewRect] = useState([]);
         const [isDrawing, setIsDrawing] = useState(true)
 
         const handleMouseDown = event => {
-            if (isDrawing && newAnnotation.length === 0) {
+            if (isDrawing && newRect.length === 0) {
                 const {x, y} = event.target.getStage().getPointerPosition();
-                setNewAnnotation([{x, y, width: 0, height: 0, key: "0"}]);
+                setNewRect([{x, y, width: 0, height: 0, key: "0"}]);
             }
         };
 
         const handleMouseUp = event => {
-            if (newAnnotation.length === 1) {
-                const sx = newAnnotation[0].x;
-                const sy = newAnnotation[0].y;
+            if (newRect.length === 1) {
+                const sx = newRect[0].x;
+                const sy = newRect[0].y;
                 const {x, y} = event.target.getStage().getPointerPosition();
-                const annotationToAdd = {
+                const rectToAdd = {
                     x: sx,
                     y: sy,
                     width: x - sx,
                     height: y - sy,
                     key: annotations.length + 1
                 };
-                annotations.push(annotationToAdd);
-                setNewAnnotation([]);
+                annotations.push({rect: rectToAdd});
+                setNewRect([]);
                 setAnnotations(annotations);
 
-                if (isDrawing && annotations.length > 1) {
-                    setArrows([1])
+                if (isDrawing) {
+                    setAnnotations(annotations.map((item) => {
+                        if (!item.text) {
+                            return {...item, text: {x: x + (x - sx) / 2, y:y - (y - sy) / 4, width: 250}}
+                        }
+                        return item
+                    }))
                 }
+
             }
         };
 
         const handleMouseMove = event => {
-            if (newAnnotation.length === 1) {
-                const sx = newAnnotation[0].x;
-                const sy = newAnnotation[0].y;
+            if (newRect.length === 1) {
+                const sx = newRect[0].x;
+                const sy = newRect[0].y;
                 const {x, y} = event.target.getStage().getPointerPosition();
-                setNewAnnotation([
+                setNewRect([
                     {
                         x: sx,
                         y: sy,
@@ -53,9 +58,6 @@ const DrawAnnotations = () => {
             }
         };
 
-        const annotationsToDraw = [...annotations, ...newAnnotation];
-
-
         const degrees_to_radians = degrees => degrees * (Math.PI / 180);
 
         const Edge = ({node1, node2}) => {
@@ -63,16 +65,16 @@ const DrawAnnotations = () => {
             const dy = node1.y - node2.y;
             let angle = Math.atan2(-dy, dx);
 
-            const radius = annotations[0].width/2;
+            const radius = annotations[0].width / 2;
 
             const arrowStart = {
-                x: node2.x ,
-                y: node2.y +(node1.width/2)
+                x: node2.x,
+                y: node2.y + (node1.width / 2)
             };
 
             const arrowEnd = {
-                x: node1.x+node1.width+4 ,
-                y: node1.y+(node1.width/2)
+                x: node1.x + node1.width + 4,
+                y: node1.y + (node1.width / 2)
             };
             // const arrowStart = {
             //     x: radius + node2.x + -radius * Math.cos(angle + Math.PI),
@@ -89,7 +91,7 @@ const DrawAnnotations = () => {
                 y: (arrowStart.y + arrowEnd.y) / 2
             };
 
-            const text = "Some text";
+            // const text = "Some text";
 
             return (
                 <Arrow
@@ -118,42 +120,83 @@ const DrawAnnotations = () => {
                 height={700}
             >
                 <Layer>
-                    {annotations.length > 1 && <Edge node1={annotations[0]} node2={annotations[1]}/>}
+                    {/*{annotations.length > 1 && <Edge node1={annotations[0]} node2={annotations[1]}/>}*/}
 
 
-                    {annotationsToDraw.map((value, index) => {
+                    {annotations.map((annotation, index) => {
+                        // if(!annotation.rect) {
+                        //     return
+                        // }
                         return (
-                            <Rect
-                                x={value.x}
-                                y={value.y}
-                                width={value.width}
-                                height={value.height}
-                                fill="transparent"
-                                stroke="#ff9955"
-                                draggable
-                                strokeWidth={4}
-                                onMouseEnter={(e) => {
-                                    const container = e.target.getStage().container();
-                                    container.style.cursor = "pointer";
-                                    setIsDrawing(false)
-                                }}
-                                onMouseLeave={(e) => {
-                                    const container = e.target.getStage().container();
-                                    container.style.cursor = "crosshair";
-                                    setIsDrawing(true)
-                                }}
-                                onDragMove={e => {
-                                    const ants = annotations.map((item, i) => {
-                                        if (i === index) return {...item, ...e.target.position()};
-                                        return item
-                                    })
-                                    setAnnotations(ants)
-                                }}
+                            <>
+                                {annotation.text && (
+                                    <Text
+                                        text="Hello there!"
+                                        x={annotation.text.x}
+                                        y={annotation.text.y}
+                                        width={annotation.text.width}
+                                        fill="red"
+                                        onMouseEnter={(e) => {
+                                            const container = e.target.getStage().container();
+                                            container.style.cursor = "pointer";
+                                            setIsDrawing(false)
+                                        }}
+                                        onMouseLeave={(e) => {
+                                            const container = e.target.getStage().container();
+                                            container.style.cursor = "crosshair";
+                                            setIsDrawing(true)
+                                        }}
+                                        draggable
+                                    />
+                                )}
+
+                                <Rect
+                                    x={annotation.rect.x}
+                                    y={annotation.rect.y}
+                                    width={annotation.rect.width}
+                                    height={annotation.rect.height}
+                                    fill="transparent"
+                                    stroke="#ff9955"
+                                    draggable
+                                    strokeWidth={4}
+                                    onMouseEnter={(e) => {
+                                        const container = e.target.getStage().container();
+                                        container.style.cursor = "pointer";
+                                        setIsDrawing(false)
+                                    }}
+                                    onMouseLeave={(e) => {
+                                        const container = e.target.getStage().container();
+                                        container.style.cursor = "crosshair";
+                                        setIsDrawing(true)
+                                    }}
+                                    onDragMove={e => {
+                                        const ants = annotations.map((item, i) => {
+                                            if (i === index) return {...item, rect: {...item.rect, ...e.target.position()}};
+                                            return item
+                                        })
+                                        setAnnotations(ants)
+                                    }}
 
 
-                            />
+                                />
+
+                            </>
                         );
                     })}
+
+                    {newRect.length > 0 && (
+                        <Rect
+                            x={newRect[0].x}
+                            y={newRect[0].y}
+                            width={newRect[0].width}
+                            height={newRect[0].height}
+                            fill="transparent"
+                            stroke="#ff6655"
+                            strokeWidth={2}
+                        />
+
+                    )}
+
                 </Layer>
             </Stage>
         );
